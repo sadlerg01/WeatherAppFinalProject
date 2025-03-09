3731a2344d89859ad583bcc11eeea9b6779285dd
Gwen <sadlerg01@gmail.com>
2025-03-09 19:21:43 -0400

    Added background picture & edited colours

--- src/index.js
+++ src/index.js
@@ -75,8 +75,9 @@ function displayForecast(response) {
                     <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
                 <div class="weather-forecast-temperatures">
                     <div class="weather-forecast-temperature">
-                        <strong>${Math.round(day.temperature.maximum)}º</strong>
+                        <strong> ${Math.round(day.temperature.maximum)}º </strong>
                     </div>
+                    <div> • </div>
               <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
             </div>
           </div>
